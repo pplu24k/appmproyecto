aaa4ecffa1f86ae54d6ae51f1826ece96d0a12c5
Jose Luis Marcos <joseluespp@gmail.com>
2023-06-22 02:02:41 +0200

    solucion error

--- src/app/shared/map/map.component.ts
+++ src/app/shared/map/map.component.ts
@@ -1,7 +1,28 @@
-import { AfterViewInit, Component, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { latLng, LatLng, MapOptions, marker, polyline, tileLayer ,Map, icon, Marker, Polyline} from 'leaflet';
+import {
+  latLng,
+  LatLng,
+  MapOptions,
+  marker,
+  polyline,
+  tileLayer,
+  Map,
+  icon,
+  Marker,
+  Polyline,
+} from 'leaflet';
 import { IncidenciasService } from 'src/app/core/services/incidencias/incidencias.service';
 import { PatientsService } from 'src/app/core/services/patients/patients.service';
 import { doorCords } from 'src/app/core/consts/doorCoord';
@@ -11,174 +32,153 @@ import { Subscription, interval } from 'rxjs';
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
-  imports: [
-    CommonModule,
-    LeafletModule
-  ]
+  imports: [CommonModule, LeafletModule],
 })
-export class MapComponent implements OnInit, OnDestroy{
-
+export class MapComponent implements OnInit, OnDestroy {
   private intervalSubscription!: Subscription;
-  map!: Map
-  poly:any
-  options :MapOptions =
-  {
+  map!: Map;
+  poly: any;
+  options: MapOptions = {
     layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 30, attribution: '...' })
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 30,
+        attribution: '...',
+      }),
     ],
     zoom: 20,
-    center: latLng(doorCords.lat,doorCords.lng)
-  }
-  @Input() puntos:any
-  @Input() trazo:Array<LatLng> = []
-  @Input() incidenciaId:any
-  renderizado = false
+    center: latLng(doorCords.lat, doorCords.lng),
+  };
+  @Input() puntos: any;
+  @Input() trazo: Array<LatLng> = [];
+  @Input() incidenciaId: any;
+  renderizado = false;
 
-  @ViewChild('map', {static: true})
+  @ViewChild('map', { static: true })
   mapContainer!: ElementRef;
 
-  constructor(private incidenciasService: IncidenciasService) { }
+  constructor(private incidenciasService: IncidenciasService) {}
 
-  cargarRuta(){
-    if( this.trazo!=null && this.trazo.length > 0){
-      if(this.map != undefined || this.map != null) {
-        this.map.remove(); 
-       } 
-      this.map = new Map(this.mapContainer.nativeElement).setView([this.trazo[0]?.lat?.valueOf(),this.trazo[0]?.lng?.valueOf() ], 25);
-      tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
-      this.poly = new Polyline(this.trazo,{
-        color: 'green',
-        weight: 5
-      })
-      this.poly.addTo(this.map)
-
-      setTimeout(()=>{this.map.invalidateSize(true)},500)
-    }
-    
-  }
-
-  recargarRuta(){
-    
-    this.getdatos()
-
-
-  }
-
-  getdatos(){
-
-    this.incidenciasService.getIncidencia(this.incidenciaId).subscribe((data:any)=> {
-
-      let recorrido = data.incidences.recorrido_paciente
-      this.trazo = []
-      for(let punto of recorrido){
-
-      
-        this.trazo.push(new LatLng(punto.latitud,punto.longitud))
+  cargarRuta() {
+    if (this.trazo != null && this.trazo.length > 0) {
+      if (this.map != undefined || this.map != null) {
+        this.map.remove();
       }
-      this.map.removeLayer(this.poly)
-      this.poly = new Polyline(this.trazo,{
+      this.map = new Map(this.mapContainer.nativeElement).setView(
+        [this.trazo[0]?.lat?.valueOf(), this.trazo[0]?.lng?.valueOf()],
+        25
+      );
+      tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(
+        this.map
+      );
+      this.poly = new Polyline(this.trazo, {
         color: 'green',
-        weight: 5
-      })
-  
-      this.poly.addTo(this.map)
-    })
+        weight: 5,
+      });
+      this.poly.addTo(this.map);
 
+      setTimeout(() => {
+        this.map.invalidateSize(true);
+      }, 500);
+    }
+  }
+
+  recargarRuta() {
+    this.getdatos();
+  }
+
+  getdatos() {
+    this.incidenciasService
+      .getIncidencia(this.incidenciaId)
+      .subscribe((data: any) => {
+        let recorrido = data.incidences.recorrido_paciente;
+        this.trazo = [];
+        for (let punto of recorrido) {
+          console.log(punto);
+          this.trazo.push(new LatLng(punto.latitud, punto.longitud));
+        }
+        this.map.removeLayer(this.poly);
+        this.poly = new Polyline(this.trazo, {
+          color: 'green',
+          weight: 5,
+        });
+
+        this.poly.addTo(this.map);
+      });
   }
 
   ngOnInit(): void {
-
-
-
-
-    if(this.puntos != null){
-      if(this.map != undefined || this.map != null) {
-        this.map.remove(); 
-       } 
-
-       if(this.puntos.length == 0){
-        this.puntos[0].lat = doorCords.lat
-        this.puntos[0].lng = doorCords.lng
-       }
-      this.map = new Map(this.mapContainer.nativeElement).setView([this.puntos[0].lat,this.puntos[0].lng],25);
-      tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
-      for(let i=0;i<this.puntos.length;i++){
-
-        if(this.puntos[i]?.patient?.ruta_foto){
-          marker([this.puntos[i].lat,this.puntos[i].lng],
-            {
-              icon: icon({
-                iconUrl:'assets/imgs/patienticon.png',
-                iconSize: [30,30]
-              })
-            }
-            ).addTo(this.map).bindPopup(`
-            <h2>
-            ${this.puntos[i].patient.nombre}
-            ${this.puntos[i].patient.apellidos}
-            </h2>
-            
-            `)
-        }
-        else{
-          marker([this.puntos[i].lat,this.puntos[i].lng],
-            {
-              icon: icon({
-                iconUrl:'assets/imgs/patienticon.png',
-                iconSize: [30,30]
-              })
-            }
-            ).addTo(this.map).bindPopup(`
-            <h2>
-            ${this.puntos[i].patient.nombre}
-            ${this.puntos[i].patient.apellidos}
-            </h2>
-            
-            `)
-        }
-
-        
-
-
+    if (this.puntos != null) {
+      if (this.map != undefined || this.map != null) {
+        this.map.remove();
       }
-      setTimeout(()=>{this.map.invalidateSize(true)},500)
+
+      if (this.puntos.length == 0) {
+        this.puntos[0].lat = doorCords.lat;
+        this.puntos[0].lng = doorCords.lng;
+      }
+      this.map = new Map(this.mapContainer.nativeElement).setView(
+        [this.puntos[0].lat, this.puntos[0].lng],
+        25
+      );
+      tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(
+        this.map
+      );
+      for (let i = 0; i < this.puntos.length; i++) {
+        if (this.puntos[i]?.patient?.ruta_foto) {
+          marker([this.puntos[i].lat, this.puntos[i].lng], {
+            icon: icon({
+              iconUrl: 'assets/imgs/patienticon.png',
+              iconSize: [30, 30],
+            }),
+          }).addTo(this.map).bindPopup(`
+            <h2>
+            ${this.puntos[i].patient.nombre}
+            ${this.puntos[i].patient.apellidos}
+            </h2>
+            
+            `);
+        } else {
+          marker([this.puntos[i].lat, this.puntos[i].lng], {
+            icon: icon({
+              iconUrl: 'assets/imgs/patienticon.png',
+              iconSize: [30, 30],
+            }),
+          }).addTo(this.map).bindPopup(`
+            <h2>
+            ${this.puntos[i].patient.nombre}
+            ${this.puntos[i].patient.apellidos}
+            </h2>
+            
+            `);
+        }
+      }
+      setTimeout(() => {
+        this.map.invalidateSize(true);
+      }, 500);
     }
 
-    if(this.trazo.length >0){
-      this.cargarRuta()
+    if (this.trazo.length > 0) {
+      this.cargarRuta();
     }
-    if(this.incidenciaId){
-      
+ 
+    if (this.incidenciaId) {
+
       this.intervalSubscription = interval(5000).subscribe(() => {
-
         this.recargarRuta();
       });
-
-      
-      
     }
 
-
-
-    marker(doorCords,{
+    marker(doorCords, {
       icon: icon({
-        iconUrl:'assets/imgs/marker.png',
-        iconSize: [20,20]
-      })
-    }).addTo(this.map)
-    
-
+        iconUrl: 'assets/imgs/marker.png',
+        iconSize: [20, 20],
+      }),
+    }).addTo(this.map);
   }
-
-
 
   ngOnDestroy() {
     if (this.intervalSubscription) {
       this.intervalSubscription.unsubscribe();
     }
   }
-
-
-
-
 }
